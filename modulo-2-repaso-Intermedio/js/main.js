6fec0cda894b6b4961f5ad9cd5870a4a6457cac9
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-08-25 18:35:02 +0200

    create html and const js to repaso intermedio

--- modulo-2-repaso-Intermedio/js/main.js
+++ modulo-2-repaso-Intermedio/js/main.js
@@ -1,1 +1,13 @@
 'use strict';
+//traer a js el h1 donde se pintará la elección
+//traer a js el body para que cambiemos la clase según el numero aleatrio
+//traer el botón de update para que genere el numero aleatorio
+const placeToFace = document.querySelector('.js_face');
+const body = document.querySelector('.js_body');
+const buttonToGenerateColor = document.querySelector('.js_button');
+
+//el botón genera un nº aleatorio
+//condición if si el nº es par no hará nada, si es impar %2 añadirá la clase .body al body
+if{
+
+}
