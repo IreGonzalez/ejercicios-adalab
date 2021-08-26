2f3fdf27730378aa6dbe930a5bd82cc38f827c6d
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-08-26 18:14:11 +0200

    lesson4 exercise1

--- modulo-2-leccion-3-ejercicio-01/js/main.js
+++ modulo-2-leccion-3-ejercicio-01/js/main.js
@@ -4,3 +4,6 @@
 const DEFAULT_AVATAR = 'http://placehold.it/300x300';
 // avatar que eligió el usuario al registrarse
 let userAvatar = 'http://www.fillmurray.com/300/300';
+
+userAvatar = '';
+
---
+++ modulo-2-leccion-4-ejercicio-01/js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//declarar las variables a utilizar del html
+
+
+//Declarar la función
+
+function multi(a, b) {
+    return a * b;
+}
+
+//Invocar la función
+multi(5, 6);
+
+
+
