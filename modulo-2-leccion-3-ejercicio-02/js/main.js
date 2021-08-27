5fb5d99034a520029bc4085f814e584c2cbf2790
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-08-27 18:58:17 +0200

    exercise 02 lesson 03

---
+++ modulo-2-leccion-3-ejercicio-02/js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//declaramos en primer lugar las variables
+
+let userName = 'Pepita';
+
+//declaramos los condicionales
+
+if (userName === 'Irene' || userName === 'Jenny') {
+    console.log(`Bienvenida, ${userName}`);
+} else {
+    console.log('Lo siento pero el usuario que has introducido no está registrado')
+}
