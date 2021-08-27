8d8a0eaddbd647531aa4d443ce6d4ac51b21f768
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-08-27 19:58:01 +0200

    exercise 05 lesson 03

--- modulo-2-leccion-3-ejercicio-05/js/main.js
+++ modulo-2-leccion-3-ejercicio-05/js/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const text = document.querySelector('.js_text');
+const title = document.querySelector('.js_title');
+const paragraph = document.querySelector('.js_paragraph');
+
+if (text.classList.contains('warning')) {
+    paragraph.innerHTML = 'Tenga cuidado';
+    title.innerHTML = 'AVISO';
+} else if (text.classList.contains('error')) {
+    paragraph.innerHTML = 'Ha surgido un error';
+    title.innerHTML = 'ERROR';
+}
+else if (text.classList.contains('success')) {
+    paragraph.innerHTML = 'Los datos son correctos';
+    title.innerHTML = 'CORRECTO';
+}
+else {
+    paragraph.innerHTML = 'NOTIFICACIÓN';
+    title.innerHTML = 'Mensaje por defecto'
+}
