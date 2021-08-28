481c890c745f5bb624f57e149d8e0d120eb0a369
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-08-28 12:05:32 +0200

    exercise 3 lesson 4

--- modulo-2-leccion-4-ejercicio-03/js/main.js
+++ modulo-2-leccion-4-ejercicio-03/js/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function calculoDeIva() {
+    const price = 8;
+    const iva = parseInt(price) * 0.21;
+    const total = price + iva;
+    return (`Precio sin Iva: ${price}, Iva: ${iva} y Total : ${total}`)
+}
+
+console.log(calculoDeIva());
