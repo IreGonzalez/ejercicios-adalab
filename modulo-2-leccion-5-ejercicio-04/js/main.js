081af3467725903d72e52db964599a6502c3a91f
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-08-28 12:40:43 +0200

    exercise 4 lesson 5

--- modulo-2-leccion-5-ejercicio-04/js/main.js
+++ modulo-2-leccion-5-ejercicio-04/js/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+//declaro las variables para poder trabajar con ellas
+
+const paragraph = document.querySelector('.js_paragraph');
+
+//declaro la función que utilizaré cuando se realice la acción o evento
+
+function changeBackgroundColor(event) {
+    paragraph.classList.add('scroll')
+}
+
+//indico el evento que hará que se ejecute la función
+
+addEventListener('scroll', changeBackgroundColor)
