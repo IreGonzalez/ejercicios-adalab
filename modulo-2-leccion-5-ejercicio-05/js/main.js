15ed5c91ece0d3abd11c503e67da57cd6d4222b2
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-08-28 13:08:25 +0200

    start exercise 5 lesson 5

--- modulo-2-leccion-5-ejercicio-05/js/main.js
+++ modulo-2-leccion-5-ejercicio-05/js/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//declaramos las variables
+
+const body = document.querySelector('.js_body');
+
+//declaramos la función a realizar cuando surja el evento
+
+function changeBackground(event) {
+
+}
