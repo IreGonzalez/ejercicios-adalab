b1fde834a37dff081bab5e24528e3ca584161b96
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-08-28 12:29:40 +0200

    try second exercise 2 lesson 5

--- modulo-2-leccion-5-ejercicio-03/js/main.js
+++ modulo-2-leccion-5-ejercicio-03/js/main.js
@@ -1,7 +1,13 @@
 'use strict'
 
 const paragraph = document.querySelector('.js-paragraph');
+
+//declaro la funcion que indica la acción de añadir un párrafo
+
+function addParagraph(event) {
+    return paragraph.innerHTML += paragraph.value;
+}
+
 //al pasar el cursor por ecima debe crear una línea de código en el html
 
-addParagraph.addEventListener('mouseover')
-addParagraph('mouseover')
+paragraph.addEventListener('mouseover', addParagraph)
