683b81792232eba71561ee3b0ba8d1d967864a39
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-08-28 11:24:02 +0200

    exercise 6 lesson 3

--- modulo-2-leccion-3-ejercicio-06/js/main.js
+++ modulo-2-leccion-3-ejercicio-06/js/main.js
@@ -1,1 +1,7 @@
 'use strict';
+
+let avocados;
+const avocadoPrice = 1.5;
+const money = 33;
+
+const operation = (money >= avocadoPrice) ? console.log(avocados = money / avocadoPrice) : console.log(avocados = 0);
