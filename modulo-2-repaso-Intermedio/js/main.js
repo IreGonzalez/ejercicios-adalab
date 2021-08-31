7ce5c0e6d65499d13a27b194846a19232b229ebc
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-08-31 21:57:59 +0200

    try second time and dont know what fail

--- modulo-2-repaso-Intermedio/js/main.js
+++ modulo-2-repaso-Intermedio/js/main.js
@@ -3,7 +3,7 @@
 //traer a js el body para que cambiemos la clase según el numero aleatrio
 //traer el botón de update para que genere el numero aleatorio
 const placeToFace = document.querySelector('.js_face');
-const body = document.querySelector('.js_body');
+const main = document.querySelector('.js_main');
 const buttonToGenerateColor = document.querySelector('.js_button');
 const moodSelect = document.querySelector('.js_mood');
 
@@ -13,4 +13,30 @@ function numberGenerator(event) {
     return console.log(Math.floor(Math.random() * 100));
 }
 
-buttonToGenerateColor.addEventListener('click', numberGenerator)
+//según sea par o impar se pondrá uno u otro fondo 
+//No funciona no se por qué
+function changeBackground(event) {
+    event.preventDefault();
+    if (numberGenerator % 2 === 0) {
+        main.classList.remove('background_orange');
+        main.classList.add('background_yellow');
+    }
+    else {
+        main.classList.remove('background_yellow');
+        main.classList.add('background_orange');
+    }
+}
+//Al elegir un mood cambia la cara
+function changeFace() {
+    const face = moodSelect.Value
+    placeToFace.innerHTML = face;
+}
+//unificamos las funciones 
+function handleGenerator() {
+    numberGenerator();
+    changeFace();
+    changeBackground();
+}
+
+//llamamos la función al hacer click
+buttonToGenerateColor.addEventListener('click', handleGenerator);
