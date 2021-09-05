169085dcb14aa691b67399ce486599187d532539
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-09-05 21:52:50 +0200

    done exercise 3 lesson 6

--- modulo-2-leccion-6-ejercicio-03/js/main.js
+++ modulo-2-leccion-6-ejercicio-03/js/main.js
@@ -1,1 +1,24 @@
-'use strict';
+'use strict'
+
+const placeToAdalaber1 = document.querySelector('.js-adalaber1');
+const placeToAdalaber2 = document.querySelector('.js-adalaber2');
+
+const adalaber1 = {}
+adalaber1.name = 'Susana',
+    adalaber1.age = 34,
+    adalaber1.job = 'Periodista';
+adalaber1.showBioAdalaber1 = function () {
+    return 'Mi nombre es' + this.name + ', tengo' + this.age + 'y soy' + this.job;
+};
+
+const adalaber2 = {}
+adalaber2.name = 'Rocío',
+    adalaber2.age = 25,
+    adalaber2.job = 'Actriz';
+
+adalaber2.showBioAdalaber2 = function () {
+    return 'Mi nombre es' + this.name + ', tengo' + this.age + 'y soy' + this.job;
+};
+
+placeToAdalaber1.innerHTML = adalaber1.showBioAdalaber1();
+placeToAdalaber2.innerHTML = adalaber2.showBioAdalaber2();
