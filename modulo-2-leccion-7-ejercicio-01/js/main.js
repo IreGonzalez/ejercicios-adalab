6a3e656673613149868d6135ca83d0ac5a69a60d
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-09-06 20:01:41 +0200

    Done exercise 1 lesson 7

--- modulo-2-leccion-7-ejercicio-01/js/main.js
+++ modulo-2-leccion-7-ejercicio-01/js/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+function workWithMovies() {
+    const movies = ['Amanece que no es poco', 'El señor de los anillos', 'Requiem por un sueño'];
+    movies[3] = 'Origen';
+    console.log(movies);
+
+    movies[3] = 'Your name';
+    console.log(movies);
+
+    movies[0] = 'El lobo de Wall street';
+    console.log(movies);
+}
+
+workWithMovies();
