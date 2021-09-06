d75d287a106d19797893d1253d4c3bbf5b17f67c
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-09-06 21:22:55 +0200

    Done exercise 1 lesson 9

--- modulo-2-leccion-9-ejercicio-01/js/main.js
+++ modulo-2-leccion-9-ejercicio-01/js/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function get100Numbers() {
+    let numbers = [];
+    for (let i = 0; i <= 100; i++) {
+        numbers.push(i);
+    }
+    console.log(numbers);
+}
+get100Numbers()
