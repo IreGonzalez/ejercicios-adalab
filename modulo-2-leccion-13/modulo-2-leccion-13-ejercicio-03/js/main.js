2d0306743a85cbe4f39a589e173afabb420d1881
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-09-07 17:35:16 +0200

    try first exercise 3 lesson 13

--- modulo-2-leccion-13/modulo-2-leccion-13-ejercicio-03/js/main.js
+++ modulo-2-leccion-13/modulo-2-leccion-13-ejercicio-03/js/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const users = [
+    { name: 'María', isPremium: false },
+    { name: 'Lucía', isPremium: true },
+    { name: 'Susana', isPremium: true },
+    { name: 'Rocío', isPremium: false },
+    { name: 'Inmaculada', isPremium: false }
+];
+//poner el saludo por defecto
+//Comprobar si es premium
+const usersSaludation = users.isPremium.map(users.name => `'Bienvenida' ${users.name}`)
+
+
+console.log(usersSaludation);
+function saludation() {
+
+    if (for (isPremium of users) === true) {
+
+    }
+}
