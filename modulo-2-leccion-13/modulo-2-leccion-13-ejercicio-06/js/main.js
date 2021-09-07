d628d4afa5c7372d632363a8e5b872d5949cc18f
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-09-07 19:19:37 +0200

    Done exercise 6 lesson 13

--- modulo-2-leccion-13/modulo-2-leccion-13-ejercicio-06/js/main.js
+++ modulo-2-leccion-13/modulo-2-leccion-13-ejercicio-06/js/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const users = [
+    { name: 'María', isPremium: false, pin: 2389 },
+    { name: 'Lucía', isPremium: true, pin: 2384 },
+    { name: 'Susana', isPremium: true, pin: 2837 },
+    { name: 'Rocío', isPremium: false, pin: 5232 },
+    { name: 'Inmaculada', isPremium: false, pin: 8998 }
+];
+
+const parPins = users.filter(user => user.pin % 2 === 0);
+console.log(parPins);
+
+
+for (const usersParPins of parPins) {
+    const accessUsers = usersParPins.name;
+    console.log(`${accessUsers} : esta usuaria tiene acceso`);
+
+}
