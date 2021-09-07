e4d602b696fcbd9a0d4f89fe55ca02c23c01cc80
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-09-07 20:33:31 +0200

    Done exercise 10 lesson 13

--- modulo-2-leccion-13/modulo-2-leccion-13-ejercicio-10/js/main.js
+++ modulo-2-leccion-13/modulo-2-leccion-13-ejercicio-10/js/main.js
@@ -1,1 +1,20 @@
 'use strict';
+const users = [
+    { name: 'María', isPremium: false, pin: 2389 },
+    { name: 'Lucía', isPremium: true, pin: 2384 },
+    { name: 'Susana', isPremium: true, pin: 2837 },
+    { name: 'Rocío', isPremium: false, pin: 5232 },
+    { name: 'Inmaculada', isPremium: false, pin: 8998 }
+];
+
+//Encontrar el nombre de la usuaria que tiene asociada la incidencia
+const impact = users.find(user => user.pin === 5232);
+console.log(impact.name);
+
+//Eliminarla del array
+const userToErase = users.findIndex(user => user.pin === 5232);
+console.log(userToErase);
+
+
+users.splice(userToErase, 1);
+console.log(users);
