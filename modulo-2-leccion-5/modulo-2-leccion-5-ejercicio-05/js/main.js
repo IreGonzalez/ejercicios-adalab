1c5c968b4632c4a240a4251473886f725f102146
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-09-08 19:56:21 +0200

    Done exercise 5 lesson 5

--- modulo-2-leccion-5/modulo-2-leccion-5-ejercicio-05/js/main.js
+++ modulo-2-leccion-5/modulo-2-leccion-5-ejercicio-05/js/main.js
@@ -7,5 +7,18 @@ const body = document.querySelector('.js_body');
 //declaramos la función a realizar cuando surja el evento
 
 function changeBackground(event) {
-
+    if (event.key === 'r') {
+        if (body.classList.contains('purple')) {
+            body.classList.remove('purple');
+        }
+        body.classList.add('red');
+    }
+    else if (event.key === 'p') {
+        if (body.classList.contains('red')) {
+            body.classList.remove('red');
+        }
+        body.classList.toggle('purple');
+    }
 }
+
+body.addEventListener('keydown', changeBackground);
