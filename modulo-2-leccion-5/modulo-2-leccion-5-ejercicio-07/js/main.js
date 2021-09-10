e32056bf031f1791535e44dc393fb12db4cfbd7f
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-09-10 16:20:51 +0200

    Done exercise 7 lesson 5

--- modulo-2-leccion-5/modulo-2-leccion-5-ejercicio-07/js/main.js
+++ modulo-2-leccion-5/modulo-2-leccion-5-ejercicio-07/js/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const button = document.querySelector('.js_button');
+
+// function handleClick() {
+//     if (button.classList.contains(`button`)) {
+//         button.classList.replace('button', 'clicked');
+//     }
+//     else if (button.classList.contains(`clicked`)) {
+//         button.classList.replace('clicked', 'button');
+//     }
+
+// }
+function handleClick() {
+    button.classList.toggle('clicked')
+}
+button.addEventListener('click', handleClick);
