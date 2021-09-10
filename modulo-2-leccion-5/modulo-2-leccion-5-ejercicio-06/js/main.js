446d2b4fe2a035e74d1ab31e4d89ce176a403a7a
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-09-10 16:02:17 +0200

    Done exercise 6 lesson 5

--- modulo-2-leccion-5/modulo-2-leccion-5-ejercicio-06/js/main.js
+++ modulo-2-leccion-5/modulo-2-leccion-5-ejercicio-06/js/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+let input = document.querySelector('.js_input');
+
+function listener(event) {
+    const value = event.currentTarget.value;
+    console.log(value);
+}
+input.addEventListener('keyup', listener);
