7b85e6744922f2a1e0876b72fd1c7cb2ad581482
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-09-10 17:34:55 +0200

    Done exercise 4 lesson 6

--- modulo-2-leccion-6/modulo-2-leccion-6-ejercicio-04/js/main.js
+++ modulo-2-leccion-6/modulo-2-leccion-6-ejercicio-04/js/main.js
@@ -1,1 +1,10 @@
 'use strict';
+const button = document.querySelector('.js_button');
+
+function listener(event) {
+    console.log(event)
+}
+
+button.addEventListener('click', listener);
+
+//La propiedad type que aàrece, creo que es un método
