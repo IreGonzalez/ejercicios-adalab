f153aacc897c30197f17eb75d78bf52d3dbb7239
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-09-20 19:04:41 +0200

    done exercise 1 lesson 3.2.4

--- modulo-3-leccion-2/src/components/App.js
+++ modulo-3-leccion-2/src/components/App.js
@@ -1,17 +1,57 @@
 
 import '../styles/App.css';
 
-function App() {
-  const handleInput = (ev) => {
-    console.log(ev.key);
-  };
-  return (
-    <div className="App">
-      <form>
-        <input type="text" onKeyUp={handleInput} />
-      </form>
-    </div>
-  );
-}
+// //EJERCICIO 3.2.1**********************************************************
 
-export default App;
+// function App() {
+//   const handleInput = (ev) => {
+//     console.log(ev.key);
+//   };
+//   return (
+//     <div className="App">
+//       <form>
+//         <input type="text" onKeyUp={handleInput} />
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// //EJERCICIO 1 3.2.4**********************************************************
+
+// import { useState } from 'react';
+
+// function App() {
+//   let number = 0;
+//   const [sum, setSum] = useState(0);
+//   const handleSum = () => {
+//     number = sum + 1;
+//     return setSum(number);
+//   }
+//   const handleSubtract = () => {
+//     number = sum - 1;
+//     return setSum(number)
+//   }
+//   const handleReset = () => {
+//     number = 0;
+//     return setSum(number);
+//   }
+//   return (
+//     <div className="App">
+//       <div>
+//         <p>Contador : {sum}</p>
+//         <button onClick={handleSum} >Aumentar</button>
+//         <button onClick={handleSubtract}>Disminuir</button>
+//         <button onClick={handleReset}>Reset</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//EJERCICIO 2 3.2.4**********************************************************
+
+
