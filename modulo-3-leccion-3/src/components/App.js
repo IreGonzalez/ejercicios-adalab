c4e4c1f86c0ecb5d1c8df235c857a8c493047aa2
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-09-21 11:34:04 +0200

    done exercise 1 lesson 3.3.1

--- modulo-3-leccion-3/src/components/App.js
+++ modulo-3-leccion-3/src/components/App.js
@@ -1,22 +1,49 @@
 
 import '../styles/App.css';
 
-//EJERCICIO 3.3.1**********************************************************
+//EJERCICIO 1 3.3.1**********************************************************
 
 import { useState } from 'react';
 
 function App() {
+  //constante de estado para cada uno de ellos
+  const [macarrones, setMacarrones] = useState();
+  const [nueces, setNueces] = useState();
+  const [patatas, setPatatas] = useState();
+  const [huevos, setHuevos] = useState();
+  const [cebolla, setCebolla] = useState();
+  const [cerveza, setCerveza] = useState();
+  //función manejadora del evento con un if ev.target es estrictamente igual que el id lo mete en el set de esos
+  const handleIngredient = (ev) => {
+    if (ev.target.id === 'macarrones') {
+      setMacarrones(ev.target.checked);
+    }
+    else if (ev.target.id === 'nueces') {
+      setNueces(ev.target.checked);
+    }
+    else if (ev.target.id === 'patatas') {
+      setPatatas(ev.target.checked);
+    }
+    else if (ev.target.id === 'huevos') {
+      setHuevos(ev.target.checked);
+    }
+    else if (ev.target.id === 'cebolla') {
+      setCebolla(ev.target.checked);
+    }
+    else if (ev.target.id === 'cerveza') {
+      setCerveza(ev.target.checked);
+    }
+  }
+  //arrow que renderiza el mensaje 
+  const renderMsgIng = () => {
+    if (cebolla && huevos && patatas && !macarrones && !nueces && !cerveza) {
+      return 'Eres una persona concebollista'
+    }
+    else {
+      return 'Eres un robot sin paladar'
+    }
+  }
 
-  // const [ingredient, setIngredient] = useState('');
-  // const handleList = (ev) => {
-  //   if () {
-  //     text = 'Eres una persona concebollista';
-  //   }
-  //   else {
-  //     text = 'Eres un robot sin paladar';
-  //   }
-
-  // }
 
   const handleSubmit = (ev) => {
     ev.preventDefault();
@@ -29,29 +56,45 @@ function App() {
       <main>
         <form onSubmit={handleSubmit}>
           <label htmlFor="macarrones">Macarrones
-            <input type="checkbox" name="macarrones" id="macarrones" value="Macarrones" />
+            <input type="checkbox" name="macarrones" id="macarrones" value="Macarrones" onChange={handleIngredient} />
           </label>
           <label htmlFor="nueces"> Nueces
-            <input type="checkbox" name="nueces" id="nueces" value="Nueces" /
-            ></label>
+            <input type="checkbox" name="nueces" id="nueces" value="Nueces" onChange={handleIngredient} /
+            >
+          </label>
           <label htmlFor="patatas">Patatas
-            <input type="checkbox" name="patatas" id="patatas" value="Patatas" />
+            <input type="checkbox" name="patatas" id="patatas" value="Patatas" onChange={handleIngredient} />
           </label>
           <label htmlFor="huevos">Huevos
-            <input type="checkbox" name="huevos" id="huevos" value="Huevos" />
+            <input type="checkbox" name="huevos" id="huevos" value="Huevos" onChange={handleIngredient} />
           </label>
           <label htmlFor="cebolla">Cebolla
-            <input type="checkbox" name="cebolla" id="cebolla" value="Cebolla" />
+            <input type="checkbox" name="cebolla" id="cebolla" value="Cebolla" onChange={handleIngredient} />
           </label>
           <label htmlFor="cerveza">Cerveza
-            <input type="checkbox" name="cerveza" id="cerveza" value="Cerveza" />
+            <input type="checkbox" name="cerveza" id="cerveza" value="Cerveza" onChange={handleIngredient} />
           </label>
           <input type="submit" value="Validar" />
         </form>
-        {/* <p>{text}</p> */}
+        <p>{renderMsgIng()}</p>
       </main>
     </div >
   );
-}
+};
 
 export default App;
+
+// //EJERCICIO 2 3.3.1**********************************************************
+
+// import { useState } from 'react';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <p></p>
+//       <textarea name="tranlator" id="translator" cols="30" rows="10" />
+//     </div>
+//   );
+// }
+
+// export default App;
