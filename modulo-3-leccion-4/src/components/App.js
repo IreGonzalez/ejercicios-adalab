0bbf6032ff19b43833f9619a09df34f931c774ac
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-09-21 17:52:13 +0200

    done exercise 1 lesson 3.4.1

--- modulo-3-leccion-4/src/components/App.js
+++ modulo-3-leccion-4/src/components/App.js
@@ -2,9 +2,26 @@
 import '../styles/App.scss';
 
 function App() {
+  const tasks = [
+    'Comprar harina, jamón y pan rallado',
+    'Hacer croquetas ricas',
+    'Ir a la puerta de un gimnasio',
+    'Comerme las croquetas mirando a la gente que entra en el gimnasio'
+  ];
+  const renderTasks = () => {
+    return tasks.map((task, index) => {
+      return (<li key={index}>
+        {task}
+      </li>);
+    });
+  };
+
   return (
     <div className="App">
-      <p>Hola Mundo</p>
+      <h1>Mi lista de tareas</h1>
+      <ol>
+        {renderTasks()}
+      </ol>
     </div>
   );
 }
