1efb88dcfd4118e85671b2810c0444700ae72860
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-09-25 13:31:27 +0200

    create file exercises lesson 6

---
+++ modulo-3-leccion-6/src/components/App.js
@@ -0,0 +1,45 @@
+
+import '../styles/App.css';
+import logo from '../images/adalab-logo-155x61.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <a href="https://adalab.es/" title="Adalab" target="-blank">
+          {/* <img
+            className="logo"
+            src="../images/adalab-logo-155x61.png"
+            alt="Adalab"
+            title="Adalab"
+          /> */}
+          <img src={logo} title="Adalab" alt="Logo de Adalab" />
+        </a>
+        <nav>
+          <ul className="navigation">
+            <li className="link">
+              <a
+                href="https://adalab.es/"
+                title="Contacto"
+                target="-blank"
+                title="Contacto"
+              >
+                Contacto
+              </a>
+            </li>
+            <li className="link">
+              <a href="https://adalab.es/" title="Blog" target="-blank" title="Blog">
+                Blog
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main className="main">
+        <h1 className="slogan">Creando diversidad digital</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
