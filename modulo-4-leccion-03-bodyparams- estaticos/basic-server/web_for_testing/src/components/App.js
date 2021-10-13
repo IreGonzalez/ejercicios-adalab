57b070e544c712f7e49a2bc53d5781f1a6ff2188
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-10-13 09:00:33 +0200

    copy files from class exercises

---
+++ modulo-4-leccion-03-bodyparams- estaticos/basic-server/web_for_testing/src/components/App.js
@@ -0,0 +1,28 @@
+import '../stylesheets/App.scss';
+
+function App() {
+
+    const handleClick = () =>{
+      fetch("http://localhost:3000/users?name=Ana", { method: "GET"})
+      .then((response)=> response.json())
+      .then((data)=>{
+        console.log(data); 
+      })
+    }
+
+
+  return (
+    <div className="App">
+      <header>
+        <h1>
+          Probando nuestro servidor
+        </h1>
+      </header>
+      <main>
+      <button onClick={handleClick}> Fetch me</button>
+      </main>
+    </div>
+  );
+}
+
+export default App;
